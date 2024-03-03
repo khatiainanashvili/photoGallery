8b4e6ef03f2b7e3b560a78ef322ecd20551c59f6
khatiainanashvili <118260235+khatiainanashvili@users.noreply.github.com>
2024-03-03 15:47:23 +0400

    Update data.ts

--- src/data/data.ts
+++ src/data/data.ts
@@ -10,7 +10,7 @@ const IMAGES_PER_PAGE= 20
 export   const fetchData = async (title : string | undefined, pageNumber: number) => {
   try {
     const { data } = await axios.get(`${API_URL}?query=${title}&page=${pageNumber}&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_ACCESS_KEY}`);
-    console.log("results",data);
+    
     return data.results;
   } catch (error :any) {
     console.error("An error occurred while fetching data:", error);
